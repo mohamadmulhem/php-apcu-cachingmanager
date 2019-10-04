47d1946753e55b418b69280984bd78ec34e7ae06
Mohamad Mulhem <dev@87try.com>
2019-10-04 22:31:54 +0300

    first release

--- CachingManager.php
+++ CachingManager.php
@@ -3,19 +3,41 @@
  * Simple caching manager depends on Alternative PHP Cache
  */
 class CachingManager {
+    /**
+     * Gets a value by $key
+     * @param $key
+     * @return mixed|null
+     */
     public static function Get($key) {
         if (apcu_exists($key))
             return apcu_fetch($key);
         else
             return null;
     }
+
+    /**
+     * Stores a key-value pair
+     * @param $key
+     * @param $value
+     */
     public static function Store($key, $value) {
         apcu_store($key, $value);
     }
+
+    /**
+     * Checks if a key-value exists
+     * @param $key
+     * @return bool
+     */
     public static function Exists($key) {
         return apcu_exists($key) 
                         && apcu_fetch($key) !== null;
     }
+
+    /**
+     * clear a key-value pair by setting it to null
+     * @param $key
+     */
     public static function Reset($key) {
         CachingManager::Store($key, null);
     }
--- test.php
+++ test.php
@@ -1,29 +1,6 @@
-# PHP  APCu CachingManager
-
-Simple caching manager depends on Alternative PHP Cache.
-
-## Requirements
-
-1. Enable APCu module in php.ini, by adding the following settings
-
-```
-extension=php_apcu.dll
-
-[APCu]
-extension=php_apcu.dll
-apc.enabled=1
-apc.shm_size=32M
-apc.ttl=7200
-apc.enable_cli=1
-apc.serializer=php
-```
-
-
-
-## Usage
-
-```python
 <?php
+require 'CachingManager.php';
+
 //store a value
 CachingManager::Store("name", "Mike");
 //check if exists
@@ -48,10 +25,3 @@ $valueExists = CachingManager::Exists("name")? 'Yes':'No';
 echo "Value Exists: " . $valueExists . "</br>";
 echo "-------- </br>";
 echo "Value: " . $value . "</br>";
-```
-
-## Contributing
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-## License
-[MIT](https://choosealicense.com/licenses/mit/)
